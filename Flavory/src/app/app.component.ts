4e5e10e27db5e9aae2f401a3ab0135da72d94417
ShakerNasser <shaker.nasser@outlook.com>
2024-03-23 23:16:04 +0100

    pages

--- Flavory/src/app/app.component.ts
+++ Flavory/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Flavory/src/app/app.routes.ts
+++ Flavory/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { MainsComponent } from './pages/mains/mains.component';
 import { DessertsComponent } from './pages/desserts/desserts.component';
 import { StartersComponent } from './pages/starters/starters.component';
 import { SiteNotFoundComponent } from './pages/site-not-found/site-not-found.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 export const routes: Routes = [
@@ -15,4 +17,6 @@ export const routes: Routes = [
 { path: 'desserts', component: DessertsComponent },
 { path: 'starters', component: StartersComponent },
 { path: 'site-not-found', component: SiteNotFoundComponent },
+{ path: 'contact', component: ContactComponent },
+{ path: 'about', component: AboutComponent },
 ];
