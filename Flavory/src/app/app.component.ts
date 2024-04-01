fe28620b7cfc1e9281b1db4b185c45f4a8bcb305
ShakerNasser <shaker.nasser@outlook.com>
2024-04-01 17:25:21 +0200

    login

--- Flavory/src/app/app.component.ts
+++ Flavory/src/app/app.component.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent, RouterLink, HomeComponent, SearchComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent, RouterLink, HomeComponent, SearchComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Flavory/src/app/pages/register/register.component.ts
+++ Flavory/src/app/pages/register/register.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { Register } from '../../models/Register.model';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerObj: Register;
+  constructor(private router: Router, private authService: AuthService) {
+    this.registerObj = new Register();
+  }
+  onRegister() {
+    console.log(this.registerObj);
+    this.authService.postRegister(this.registerObj)?.subscribe({
+      next: (res) => {
+        console.log(res);
+        if (res.token) {
+          localStorage.setItem('token', res.token);
 
+          console.log(localStorage.getItem('token'));
+
+          alert('Register Success');
+          this.router.navigateByUrl('/home')
+
+        } else {
+          alert(res.message);
+        }
+      }
+    })
+  }
 }
